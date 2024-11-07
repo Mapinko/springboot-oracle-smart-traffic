b75295844ffb0a2e64cc51cec6e54cb15e0b8deb
Mari Spinola <mapincosf@gmail.com>
2024-11-06 21:54:32 -0300

    feat: project structure

---
+++ smart-traffic/src/main/java/br/com/fiap/smarttraffic/SmartTrafficApplication.java
@@ -0,0 +1,14 @@
+package br.com.fiap.smarttraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTrafficApplication.class, args);
+	}
+
+}
+	
